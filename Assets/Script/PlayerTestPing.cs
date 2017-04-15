c2f8d62d48aff6cbebc1895df8a8f2d94eac0feb
ThinhHB <hbthinh176177@gmail.com>
2017-04-15 10:43:38 +0700

    - Create project

---
+++ Assets/Script/PlayerTestPing.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+[NetworkSettings(channel = 1)]
+public class PlayerTestPing : NetworkBehaviour
+{
+	#region Config
+	[SerializeField] KeyCode testCmdRpcKey = KeyCode.A;
+	[SerializeField] KeyCode testMessageKey = KeyCode.S;
+	#endregion//config
+
+
+	#region Network callbacks
+	public override void OnStartLocalPlayer ()
+	{
+		// this enable the Update() function
+		enabled = true;
+		// reigster to receive event
+		NetworkManager.singleton.client.RegisterHandler(PING_MESSAGE, OnLocalReceiveCheckPingMessage);
+	}
+
+	public override void OnStartClient ()
+	{
+		enabled = false;
+	}
+
+	public override void OnStartServer ()
+	{
+		enabled = false;
+		NetworkServer.RegisterHandler(PING_MESSAGE, OnServerReceiveCheckPingMessage);
+	}
+	#endregion // Network callbacks
+
+
+	#region Local
+	void LocalCheckPingViaCmdRpc()
+	{
+		_sentTime = Time.time;
+		CmdServerReceiveCheckingPingRequest();
+	}
+
+	void LocalCheckPingViaMessage()
+	{
+		_sentTime = Time.time;
+		var client = NetworkManager.singleton.client;
+		var message = new PingMessage();
+		client.Send(PING_MESSAGE, message);
+	}
+
+	void OnLocalReceiveCheckPingMessage(NetworkMessage message)
+	{
+		var RTT = (Time.time - _sentTime) * 1000f;
+		Debug.LogFormat("NetworkMessag -> RTT : {0}", RTT);
+	}
+	#endregion
+
+
+	#region Server
+	void OnServerReceiveCheckPingMessage(NetworkMessage message)
+	{
+		var connectionID = message.conn.connectionId;
+		var reply = new PingMessage();
+		NetworkServer.SendToClient(connectionID, PING_MESSAGE, reply);
+	}
+	#endregion
+
+
+	#region Private
+	void Update()
+	{
+		if (Input.GetKeyDown(testCmdRpcKey))
+		{
+			LocalCheckPingViaCmdRpc();
+		}
+
+		if (Input.GetKeyDown(testMessageKey))
+		{
+			LocalCheckPingViaMessage();
+		}
+	}
+	#endregion//Private
+
+
+	#region CheckPing via Cmd RPC
+	float _sentTime;
+	[Command]
+	void CmdServerReceiveCheckingPingRequest()
+	{
+		RpcClientReceiveCheckingPingMessageFromServer();
+	}
+
+	[ClientRpc]
+	void RpcClientReceiveCheckingPingMessageFromServer()
+	{
+		if (isLocalPlayer)
+		{
+			var RTT = (Time.time - _sentTime) * 1000f;
+			Debug.LogFormat("Cmd, Rpc -> RTT : {0}", RTT);
+		}
+	}
+	#endregion//CheckPing via Cmd RPC
+
+
+	#region Custom classes
+	const short PING_MESSAGE = 1000;
+
+	[System.Serializable]
+	public class PingMessage : MessageBase
+	{
+	}
+	#endregion//Custom classes
+}
+
---
+++ Assets/Script/UIMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Use in menu scene, have some public functions. use them as target for
+/// 2 button on screen : StartLocalServer and ConnectLocalServer.
+/// </summary>
+public class UIMenu : MonoBehaviour
+{
+	#region Init, config
+	#endregion//Init, config
+
+	#region Public, Inspector
+	public void IN_PressStartLocalServer()
+	{
+		var netManager = NetworkManager.singleton;
+		netManager.networkAddress = "localhost";
+		netManager.networkPort = 7777;
+		netManager.StartServer();
+	}
+
+	public void IN_PressConnectLocal()
+	{
+		var netManager = NetworkManager.singleton;
+		netManager.networkAddress = "localhost";
+		netManager.networkPort = 7777;
+		netManager.StartClient();
+	}
+	#endregion//Public, Inspector
+}
